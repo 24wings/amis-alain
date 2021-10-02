6376e143c670b4ca4261ef7aa6e3e4e3d0e84dba
topshadow <2121718893@qq.com>
2021-10-02 21:35:26 +0800

    first

---
+++ src/style-icons.ts
@@ -0,0 +1,13 @@
+// Custom icon static resources
+
+import {
+  BulbOutline,
+  ExceptionOutline,
+  InfoOutline,
+  LinkOutline,
+  ProfileOutline,
+  QuestionOutline,
+  QuestionCircleFill
+} from '@ant-design/icons-angular/icons';
+
+export const ICONS = [InfoOutline, BulbOutline, ProfileOutline, ExceptionOutline, LinkOutline, QuestionOutline, QuestionCircleFill];
